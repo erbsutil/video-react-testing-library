db55e0db17194e4d04b08b8dd447637aaa474dde
Erick Sutil <ericksutil@gmail.com>
2021-05-29 06:02:45 -0300

    Creating a simple ToDo for tests

---
+++ src/components/Todo.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const Todo = () =>  {
+    const [task, updateTask] = useState('')
+    const [tasks, updateTasks] = useState([])
+
+    const handleInputChange = event => updateTask(event.target.value)
+
+    const handleFormSubmit = event => {
+        event.preventDefault()
+        if (task.trim()) {
+            updateTasks([...tasks, task])
+            updateTask('')
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleFormSubmit}>
+                <input
+                    onChange={handleInputChange}
+                    placeholder="New task"
+                    type="text"
+                    value={task}
+                />
+                <button type="submit">Add new task</button>
+            </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Task</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { tasks.map((t, index) => (
+                        <tr key={index}>
+                            <td>{t}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Todo
